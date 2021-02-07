1cd2d3cb3d2fd57e4cc28eed69aebe4fdad118fb
Laura Aupert <aupert.laura@gmail.com>
2021-02-06 19:22:31 -0500

    Created .env file for environment variables.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer")
+var dotenv = require("dotenv").config()
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+//   host: "localhost",
   port: 3306,
-  user: "root",
-  password: "",
+//   user: "root",
+//   password: "",
   database: "employeetracker.sql"
 });
 
